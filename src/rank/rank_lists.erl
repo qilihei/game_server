d2feab4c79db3d89e11c67e1c255b47cd6bd6cfc
xzj <xianzijie@shiyue.com>
2023-03-31 01:35:34 +0800

    11

--- src/rank/rank_lists.erl
+++ src/rank/rank_lists.erl
@@ -8,10 +8,12 @@
 %%%-------------------------------------------------------------------
 -module(rank_lists).
 
+-include("rank.hrl").
+
 -export([
-    create_rank/1
+    create_rank/2
     ,inset_test/2
-    ,test/2
+    ,test/1
 
 ]).
 
@@ -19,28 +21,32 @@ unixtime() ->
     {M, S, _} = os:timestamp(),
     M * 1000000 + S.
 
-create_rank(Num) ->
-    F = fun(N) ->
-        Order = #rank_order{key = {N, unixtime()}, id = N},
-        Text = #rank_text{id = N, text = lists:seq(0, N)},
-        ets:insert(?ets_rank_order, Order),
-        ets:insert(?ets_rank_text, Text)
+create_rank(Num, State) ->
+    F = fun(N, Acc) ->
+        Info = #rank_info{id = N, value = N, text = lists:seq(0, N)},
+        [Info | Acc]
         end,
-    lists:foreach(F, lists:seq(1, Num)).
+    RankList = lists:foldl(F, [], lists:seq(1, Num)),
+    {ok, State#rank_mgr_state{rank_list = RankList}}.
 
-inset_test(Id, Key) ->
-    Order = #rank_order{key = {Key, unixtime()}, id = Id},
-    Text = #rank_text{id = Id, text = lists:seq(0, Id)},
-    ets:insert(?ets_rank_order, Order),
-    ets:insert(?ets_rank_text, Text).
+inset_test(N, State = #rank_mgr_state{rank_list = RankList}) ->
+    Info = #rank_info{id = N, value = N, text = lists:seq(0, N)},
+    NewRankList = test([Info, RankList]),
+    NewState = State#rank_mgr_state{rank_list = NewRankList},
+    {ok, NewState}.
 
-test(Id, Key) ->
-    inset_test(Id, Key),
-    F = fun(Order = #rank_order{id = Id}, {Rank, Acc}) ->
-        NewOrder = Order#rank_order{rank = Rank},
-        {Rank + 1, [NewOrder | Acc]}
-        end,
-    ets:foldl(F, {1, []}, ?ets_rank_order),
+
+
+test(RankList) ->
+    NewRankList = sort(RankList),
     {_, Time1} = statistics(runtime),
     {_, Time2} = statistics(walk_clock),
-    io:format("Time1 = ~w~n, Time2 = ~w~n",[Time1, Time2]).
+    io:format("Time1 = ~w~n, Time2 = ~w~n",[Time1, Time2]),
+    NewRankList.
+
+sort(RankList) ->
+    [ || RankList]
+    do_sort([]).
+
+check(#rank_info{value = Value1}, #rank_info{value = Value2}) ->
+    Value1 > Value2.
